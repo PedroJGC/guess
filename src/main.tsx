131bf1d0171b56d0916d4eb2f9c8d34eb938f87d
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-15 21:10:24 -0300

    feat: update HTML structure and styles, remove unused CSS, and add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: ignore */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './global.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
