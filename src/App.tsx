65399916444b955d2ea047a401a7e83e172fd2b1
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-17 21:12:48 -0300

    feat: implement Letter component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './app.module.css'
 import { Header } from './components/Header'
+import { Letter } from './components/Letter'
 import { Tip } from './components/Tip'
 
 function App() {
@@ -13,6 +14,12 @@ function App() {
         <Header current={5} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
+
+        <Letter value="R" />
+        <Letter value="E" />
+        <Letter value="A" />
+        <Letter value="C" />
+        <Letter value="T" />
       </main>
     </div>
   )
