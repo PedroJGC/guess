fc174b3d41abc9bb126ef1ca24da20c039f23dbf
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-21 18:08:32 -0300

    feat: letter size variations

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,9 +1,15 @@
+import { Letter } from '../Letter'
 import styles from './style.module.css'
 
 export function LettersUsed() {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras utilizadas</h5>
+
+      <div>
+        <Letter size="small" value="R" />
+        <Letter size="small" value="X" />
+      </div>
     </div>
   )
 }
