41cbd25bb17cc6a021836f5498a792da09eaecae
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-22 17:56:26 -0300

    feat: displaying letter of the word

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import styles from './app.module.css'
 import { Button } from './components/Button'
 import { Header } from './components/Header'
@@ -5,25 +6,46 @@ import { Input } from './components/Input'
 import { Letter } from './components/Letter'
 import { LettersUsed } from './components/LettersUsed'
 import { Tip } from './components/Tip'
+import { type Challenge, WORDS } from './utils/words'
 
-function App() {
+export default function App() {
+  const [letter, setLetter] = useState('')
+  const [attempts, setAttempts] = useState(0)
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
+
   function handleRestartGame() {
     alert('Reiniciar o jogo!')
+  }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter('')
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
+  if (!challenge) {
+    return
   }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
 
         <div className={styles.word}>
-          <Letter value="R" />
-          <Letter value="E" />
-          <Letter value="A" />
-          <Letter value="C" />
-          <Letter value="T" />
+          {challenge.word.split('').map(() => (
+            <Letter value="" />
+          ))}
         </div>
 
         <h4>Palpite</h4>
@@ -38,5 +60,3 @@ function App() {
     </div>
   )
 }
-
-export default App
--- src/utils/words.ts
+++ src/utils/words.ts
@@ -1,10 +1,10 @@
-export type Challenges = {
+export type Challenge = {
   id: number
   word: string
   tip: string
 }
 
-export const WORDS: Challenges[] = [
+export const WORDS: Challenge[] = [
   { id: 1, word: 'CSS', tip: 'Linguagem de estilos' },
   { id: 2, word: 'REACT', tip: 'Biblioteca para criar interfaces web' },
   { id: 3, word: 'HTML', tip: 'Linguagem de marcação' },
