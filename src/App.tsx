37db56ac4506795bc8edfa3146a05129a5d81955
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-23 18:34:12 -0300

    feat: creating handleConfirm function

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,29 @@ export default function App() {
     setLetter('')
   }
 
+  function handleConfirm() {
+    if (!challenge) {
+      return
+    }
+
+    if (!letter.trim()) {
+      return alert('Digite uma letra!')
+    }
+
+    const value = letter.toUpperCase()
+    const exists = lettersUsed.find(
+      (used) => used.value.toUpperCase() === value
+    )
+
+    if (exists) {
+      return alert(`Você já utilizou a letra ${value}`)
+    }
+
+    setLettersUsed((prevState) => [...prevState, { value, correct: false }])
+
+    setLetter('')
+  }
+
   useEffect(() => {
     startGame()
   }, [])
@@ -52,8 +75,14 @@ export default function App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar" />
+          <Input
+            autoFocus
+            maxLength={1}
+            onChange={(e) => setLetter(e.target.value)}
+            placeholder="?"
+            value={letter}
+          />
+          <Button onClick={handleConfirm} title="Confirmar" />
         </div>
 
         <LettersUsed data={lettersUsed} />
