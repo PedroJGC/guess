af19f51f761753f88c15cfb20260a03b327b3e0d
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-23 18:45:13 -0300

    feat: checking user hits

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Tip } from './components/Tip'
 import { type Challenge, WORDS } from './utils/words'
 
 export default function App() {
+  const [score, setScore] = useState(0)
   const [letter, setLetter] = useState('')
   const [attempts, setAttempts] = useState(0)
   const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([])
@@ -46,7 +47,16 @@ export default function App() {
       return alert(`Você já utilizou a letra ${value}`)
     }
 
-    setLettersUsed((prevState) => [...prevState, { value, correct: false }])
+    const hits = challenge.word
+      .toLowerCase()
+      .split('')
+      .filter((char) => char === value).length
+
+    const correct = hits > 0
+    const currentScore = score + hits
+
+    setLettersUsed((prevState) => [...prevState, { value, correct }])
+    setScore(currentScore)
 
     setLetter('')
   }
@@ -64,7 +74,7 @@ export default function App() {
       <main>
         <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
-        <Tip tip="Uma das linguagens de programação mais utilizadas" />
+        <Tip tip={challenge.tip} />
 
         <div className={styles.word}>
           {challenge.word.split('').map(() => (
